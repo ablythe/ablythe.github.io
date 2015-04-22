28162b742ab5e4fac15fa861fc271566b08432be
Alexander Blythe <Alexander.blythe@gmail.com>
2015-04-22 11:56:32 -0400

    Switched templates

--- js/init.js
+++ js/init.js
@@ -1,87 +1,119 @@
 /*
-	Transit by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+	Spectral by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	skel.init({
-		reset: 'full',
-		breakpoints: {
-			global: {
-				href: 'css/style.css',
-				containers: 1400,
-				grid: { gutters: ['2em', 0] }
-			},
-			xlarge: {
-				media: '(max-width: 1680px)',
-				href: 'css/style-xlarge.css',
-				containers: 1200
-			},
-			large: {
-				media: '(max-width: 1280px)',
-				href: 'css/style-large.css',
-				containers: 960,
-				grid: { gutters: ['1.5em', 0] },
-				viewport: { scalable: false }
-			},
-			medium: {
-				media: '(max-width: 980px)',
-				href: 'css/style-medium.css',
-				containers: '90%!'
-			},
-			small: {
-				media: '(max-width: 736px)',
-				href: 'css/style-small.css',
-				containers: '90%!',
-				grid: { gutters: ['1.25em', 0] }
-			},
-			xsmall: {
-				media: '(max-width: 480px)',
-				href: 'css/style-xsmall.css'
-			}
-		},
-		plugins: {
-			layers: {
-				config: {
-					mode: 'transform'
-				},
-				navButton: {
-					breakpoints: 'medium',
-					height: '4em',
-					html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span>',
-					position: 'top-left',
-					side: 'top',
-					width: '6em'
-				},
-				navPanel: {
-					animation: 'overlayX',
-					breakpoints: 'medium',
-					clickToHide: true,
-					height: '100%',
-					hidden: true,
-					html: '<div data-action="moveElement" data-args="nav"></div>',
-					orientation: 'vertical',
-					position: 'top-left',
-					side: 'left',
-					width: 250
-				}
-			}
-		}
-	});
+	skel
+		.breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
 
 	$(function() {
 
 		var	$window = $(window),
-			$body = $('body');
+			$body = $('body'),
+			$wrapper = $('#page-wrapper'),
+			$banner = $('#banner'),
+			$header = $('#header');
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
 
 			$window.on('load', function() {
-				$body.removeClass('is-loading');
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
 			});
+
+		// Mobile?
+			if (skel.vars.isMobile)
+				$body.addClass('is-mobile');
+			else
+				skel
+					.on('-medium !medium', function() {
+						$body.removeClass('is-mobile');
+					})
+					.on('+medium', function() {
+						$body.addClass('is-mobile');
+					});
+
+		// Scrolly.
+			$('.scrolly')
+				.scrolly({
+					speed: 1500,
+					offset: $header.outerHeight()
+				});
+
+		// Menu.
+			var	$menu = $('#menu'),
+				$menuClose = $('<a class="close">').appendTo($menu),
+				$menuToggle = $('.menuToggle');
+
+			// Move to end of body.
+				$menu
+					.appendTo($body);
+
+			// Close.
+				$menuClose
+					.on('click touchend', function(event) {
+
+						event.preventDefault();
+						event.stopPropagation();
+
+						$body.removeClass('is-menu-visible');
+
+					});
+
+			// Toggle.
+				$menuToggle
+					.on('click touchend', function(event) {
+
+						event.preventDefault();
+						event.stopPropagation();
+
+						$body.toggleClass('is-menu-visible');
+
+					});
+
+			// Wrapper.
+				$wrapper
+					.on('click touchend', function(event) {
+
+						if ($body.hasClass('is-menu-visible')) {
+
+							event.preventDefault();
+							event.stopPropagation();
+
+							$body.removeClass('is-menu-visible');
+
+						}
+
+					});
+
+		// Header.
+			if (skel.vars.IEVersion < 9)
+				$header.removeClass('alt');
+
+			if ($banner.length > 0
+			&&	$header.hasClass('alt')) {
+
+				$window.on('resize', function() { $window.trigger('scroll'); });
+
+				$banner.scrollex({
+					bottom:		$header.outerHeight() + 1,
+					terminate:	function() { $header.removeClass('alt'); },
+					enter:		function() { $header.addClass('alt'); },
+					leave:		function() { $header.removeClass('alt'); }
+				});
+
+			}
 
 	});
 
